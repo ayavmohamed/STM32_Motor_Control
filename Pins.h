03cf6c1d813a42a3b136aaf65f5d876ed3fc7488
Aya <ayamohamedabdalrahman70@gmail.com>
2026-08-22 12:56:38 +0300

    Add pins

---
+++ Pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+// L298N
+#define L298N_IN1 PB12
+#define L298N_IN2 PB13
+#define L298N_ENA PA6
+
+// Cytron MD10A
+#define CYTRON_DIR PB14
+#define CYTRON_PWM PA7
+
+// BTS7960
+#define BTS_RPWM PB6
+#define BTS_LPWM PB7
+
+#endif
